003bcfc1bd3acad607d5ba07c6a439e4f5761b93
lawre <t.m.laurenzo@gmail.com>
2024-09-14 23:30:44 +0100

    Chapter-6 complete

--- CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees/CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -14,9 +14,15 @@ public class CompaniesController : ControllerBase
     [HttpGet]
     public IActionResult GetCompanies()
     {
-        throw new Exception("Exception");
         var companies = _service.CompanyService.GetAllCompanies(trackChanges: false);
 
         return Ok(companies);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetCompany(Guid id)
+    {
+        var company = _service.CompanyService.GetCompany(id, trackChanges: false);
+        return Ok(company);
     }
 }
